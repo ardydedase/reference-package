f497e9953cc6991791fd7411d6906694c6f17776
Ardy Dedase <ardy.dedase@skyscanner.net>
2020-04-20 10:31:05 +0800

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import fetch from 'node-fetch';
+
+export class GitHub {
+  username: string;
+  constructor(username: string) {
+    this.username = username;
+  }
+
+  async getRepos() {
+    const response = await fetch(`https://api.github.com/users/${this.username}/repos`);
+
+    return response;
+  }
+}
